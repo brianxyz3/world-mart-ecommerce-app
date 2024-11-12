0a294592a4dcfafa23d2bfeb685d9a54a58211a2
BrianXYZ <brianchima22@gmail.com>
2024-11-12 12:04:29 +0100

    edited hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
     return (
         <>
-            <main className="text-gray-800 text-center w-full mt-9">
+            <main className="container text-gray-800 text-center mt-9">
                 <div className="text-6xl font-bold">
                     <p>Fulfill Your Shopping</p>
                     <p className="my-2">Dreams with World Mart</p>
@@ -13,9 +13,9 @@ const Hero = () => {
                 </div>
                 <button className="border rounded bg-blue-800 mr-2 py-1 px-5 text-blue-100">View Products</button>
                 <button className="border border-blue-800 rounded py-1 px-5 text-blue-800 ml-2">Exciting Offers</button>
-                <div className="flex justify-between w-full">
+                <div className="flex">
                     <img src="../src/assets/images/shopping-cart-1.avif" alt="a shopping cart filled with packages" className="w-1/5" />
-                    <div className="flex flex-row items-center -mt-8 ms-auto w-1/2">
+                    <div className="flex flex-row items-center -mt-8 ms-auto w-1/2 -me-32">
                         <img src="../src/assets/images/wm-hero-img.jpg" alt="" className="w-1/2 -me-14" />
                         <div className="-rotate-90 p-1 text-3xl bg-blue-900 rounded-tr-2xl">
                             <p className="text-blue-950">WORLD MART</p>
